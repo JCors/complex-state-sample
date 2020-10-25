ea4ca426a41bd7825d9c04191e16109a2948320c
Joshua Corona <joshuacorona33@gmail.com>
2020-10-25 17:41:41 +0800

    Add input data using hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,51 +4,67 @@ import React, { useState } from "react";
 
 function App() {
 
+    // Create destructing for Array for first name, last name, and email
+	const [contact, setContact] = useState({
+		fName: "",
+		lName: "",
+		email: "",
+	});
 
-    const [fullName, setFullName] = useState({
-			fName: "",
-			lName: "",
+	// Create function for handleChange
+	function handleChange(event) {
+		//apply destructing for the object
+		const { name, value } = event.target;
+
+		// create statefull
+		setContact((prevValue) => {
+			if (name === "fName") {
+				return {
+					fName: value,
+                    lName: prevValue.lName,
+                    email: prevValue.email
+				};
+			} else if (name === "lName") {
+				return {
+					fName: prevValue.fName,
+					lName: value,
+					email: prevValue.email,
+				};
+			} else if (name === "email") {
+				return {
+					fName: prevValue.fName,
+					lName: prevValue.lName,
+					email: value,
+				};
+			}
 		});
+	}
 
-		// Create function for handleChange
-    function handleChange(event) {
-            
-			//apply destructing for the object
-			const { value, name } = event.target;
-
-			// create statefull
-			setFullName((prevValue) => {
-				if (name === "fName") {
-					return {
-						fName: value,
-						lName: prevValue.lName,
-					};
-				} else if (name === "lName") {
-					return {
-						fName: prevValue.fName,
-						lName: value,
-					};
-				}
-			});
-		}
-    
 	return (
 		<div className="container">
 			<h1>
-				Hello {fullName.fName} {fullName.lName}{" "}
+				Hello {contact.fName} {contact.lName}{" "}
 			</h1>
+			<p>{contact.email}</p>
 			<form>
 				<input
 					name="fName"
 					onChange={handleChange}
 					placeholder="First Name"
-					value={fullName.fName}
+					value={contact.fName}
 				/>
-                <input
-                    name="lName"
-                    onChange={handleChange}
-                    placeholder="Last Name"
-                    value={fullName.lName}
+				<input
+					name="lName"
+					onChange={handleChange}
+					placeholder="Last Name"
+					value={contact.lName}
+				/>
+
+				<input
+					name="email"
+					onChange={handleChange}
+					placeholder="Email"
+					value={contact.email}
 				/>
 				<button>Submit</button>
 			</form>
